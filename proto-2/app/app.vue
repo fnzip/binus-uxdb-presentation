ad6e626bbba65294fc7965a3f10295b94ef00adb
Alfian Oktafireza <alfianokt104@gmail.com>
2026-05-06 18:45:07 +0700

    feat: add theme switch button to the app layout

--- proto-2/app/app.vue
+++ proto-2/app/app.vue
@@ -28,6 +28,11 @@ useSeoMeta({
       <NuxtPage />
     </UMain>
 
+    <!-- Theme Switch -->
+    <div class="fixed top-3 right-3 z-50">
+      <UColorModeButton />
+    </div>
+
     <!-- Mobile Bottom Navigation -->
     <nav class="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 z-50 lg:hidden">
       <div class="flex items-center justify-around py-2">
